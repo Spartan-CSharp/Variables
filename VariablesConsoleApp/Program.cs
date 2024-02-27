6f784f9b5e360aa838c041396e4bf4e0c9b19369
Pierre Plourde <pierre@spartancsharp.net>
2024-02-27 09:20:32 -0500

    Create Code
    Create a Console Application that has variables to hold a person's name, age, if they are alive, and their phone number. We do not need to capture these values from the user.

--- VariablesConsoleApp/Program.cs
+++ VariablesConsoleApp/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string firstName = string.Empty;
+string lastName = string.Empty;
+int age = 0;
+bool isAlive = false;
+string phoneNumber = string.Empty;
+
+firstName = "Pierre";
+lastName = "Plourde";
+age = 48;
+isAlive = true;
+phoneNumber = "905-439-7645";
+
+Console.WriteLine($"{firstName} {lastName} is {age} years old.");
+Console.WriteLine($"Is {firstName} alive: {isAlive}");
+Console.WriteLine($"{firstName}'s phone number is {phoneNumber}");
