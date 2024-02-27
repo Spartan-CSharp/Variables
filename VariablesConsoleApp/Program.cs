64d1fb3089b7873819cb8001ad2d84b5837eab5a
Pierre Plourde <pierre@spartancsharp.net>
2024-02-27 09:22:05 -0500

    Code Cleanup
    Clear Errors, Warnings, and Messages; run Code Cleanup Profile

--- VariablesConsoleApp/Program.cs
+++ VariablesConsoleApp/Program.cs
@@ -1,14 +1,8 @@
-﻿string firstName = string.Empty;
-string lastName = string.Empty;
-int age = 0;
-bool isAlive = false;
-string phoneNumber = string.Empty;
-
-firstName = "Pierre";
-lastName = "Plourde";
-age = 48;
-isAlive = true;
-phoneNumber = "905-439-7645";
+﻿string firstName = "Pierre";
+string lastName = "Plourde";
+int age = 48;
+bool isAlive = true;
+string phoneNumber = "905-439-7645";
 
 Console.WriteLine($"{firstName} {lastName} is {age} years old.");
 Console.WriteLine($"Is {firstName} alive: {isAlive}");
